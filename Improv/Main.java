f9fc45a855103a6390410b8c9733083a5bb4321a
delanoismynameo <ste20059@byui.edu>
2026-04-07 02:49:18 -0600

    new and improved

--- Improv/Main.java
+++ Improv/Main.java
@@ -1,15 +1,22 @@
 package Improv;
 
 import java.util.Scanner;
+import java.io.File;         
+import java.io.FileWriter;     
+import java.io.IOException;     
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter; 
 
 public class Main {
     public static void main(String[] args) {
         Scanner userSelection = new Scanner(System.in);
         RandomKey keyPicker = new RandomKey();
         int menuOption = 0;
-        while (menuOption != 2) {
-            System.out.println("Welcome to the piano improvization assistant!\nWhat would you like to do? (enter number");
-            System.out.println("1. Random Key\n2. Exit");
+        String fileName = "practice_log.txt";
+
+        while (menuOption != 3) { 
+            System.out.println("Welcome to the piano improvization assistant!\nWhat would you like to do? (enter number)");
+            System.out.println("1. Random Key\n2. View Practice History\n3. Exit"); 
 
             if (userSelection.hasNextInt()) {
                 menuOption = userSelection.nextInt();
@@ -19,14 +26,49 @@ public class Main {
                     String key = keyPicker.getRandomKey();
                     System.out.println("\n  Your key is: " + key);
 
+                    try {
+                        FileWriter writer = new FileWriter(fileName, true); 
+                        LocalDateTime now = LocalDateTime.now();
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                        
+                        writer.write(now.format(formatter) + " - Practiced: " + key + "\n");
+                        writer.close();
+                    } catch (IOException e) {
+                        System.out.println("An error occurred while saving to the log.");
+                        e.printStackTrace(); 
+                    }
+
                     System.out.println("\nPress Enter to return to the menu...");
                     userSelection.nextLine();
-                    // set menuOption back to zero to keep the key loop alive
-                    menuOption= 0;
+                    menuOption = 0;
+                    
+                } else if (menuOption == 2) {
+                   
+                    System.out.println("\n--- Practice History ---");
+                    try {
+                        File logFile = new File(fileName);
+                        if (logFile.exists()) {
+                            Scanner fileReader = new Scanner(logFile);
+                            while (fileReader.hasNextLine()) {
+                                System.out.println(fileReader.nextLine());
+                            }
+                            fileReader.close();
+                        } else {
+                            System.out.println("No practice history found yet! Generate a key first.");
+                        }
+                    } catch (IOException e) {
+                        System.out.println("An error occurred while reading the log.");
+                        e.printStackTrace();
+                    }
+                    
+                    System.out.println("------------------------");
+                    System.out.println("Press Enter to return to the menu...");
+                    userSelection.nextLine();
+                    menuOption = 0;
                 }
             } else {
                 System.out.println("Invalid input. Please enter a number.\n");
-                userSelection.next(); // Clear the invalid input
+                userSelection.next(); 
             }
         }
         System.out.println("See ya next time!");
--- Improv/RandomKey.java
+++ Improv/RandomKey.java
@@ -1,15 +1,18 @@
 package Improv;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class RandomKey {
     // 24 Major and Minor Keys
-    String[] allKeys = {
+    ArrayList<String> allKeys = new ArrayList<>(Arrays.asList(
         "C Major", "G Major", "D Major", "A Major", "E Major", "B Major", 
         "F# Major", "C# Major", "F Major", "Bb Major", "Eb Major", "Ab Major",
         "A Minor", "E Minor", "B Minor", "F# Minor", "C# Minor", "G# Minor", 
         "D# Minor", "A# Minor", "D Minor", "G Minor", "C Minor", "F Minor"
-    };
+    ));
     public String getRandomKey() {
-        int index = (int) (Math.random() * allKeys.length);
-        return allKeys[index];
+        int index = (int) (Math.random() * allKeys.size());
+        return allKeys.get(index);
     }
 }
